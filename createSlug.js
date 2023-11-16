59995547f3041045b0d12692e799664795035f8b
Andrea Fermo <andreafermo.sd@gmail.com>
2023-11-16 16:47:31 +0100

    refactoring createSlug.js

--- createSlug.js
+++ createSlug.js
@@ -1,24 +1,36 @@
 module.exports = function (title, array) {
-    if (!title || typeof title != "string") {
-        throw new Error("kebab-case: il titolo non è presente o non è una stringa");
-    } else if (!array) {
-        throw new Error("kebab-case: devi passare un array come secondo argomento");
-    };
+    /* ESERCIZIO PRIMA DEL REFACTORING
+        if (!title || typeof title != "string") {
+            throw new Error("kebab-case: il titolo non è presente o non è una stringa");
+        } else if (!array) {
+            throw new Error("kebab-case: devi passare un array come secondo argomento");
+        };
 
-    let slug = title.toLowerCase().trim().replaceAll(" ", "-");
+        let slug = title.toLowerCase().trim().replaceAll(" ", "-");
+
+        let counter = 2;
+        let myString = slug;
+        const mySlugArray = array.map((element) => element.slug.valueOf());
+
+        while (mySlugArray.includes(myString)) {
+            myString = `${slug}-${counter}`
+            if (!mySlugArray.includes(myString)) {
+                slug = myString;
+            } else {
+                counter++;
+            }
+        };
+
+        return slug;
+    */
+    if (!title || typeof title !== "string") throw new Error("kebab-case: il titolo non è presente o non è una stringa");
+    if (!array) throw new Error("kebab-case: devi passare un array come secondo argomento");
+
+    let slug = title.toLowerCase().trim().replace(/\s+/g, "-");
+    const existingSlugs = array.map((element) => element.slug.valueOf());
 
     let counter = 2;
-    let myString = slug;
-    const mySlugArray = array.map((element) => element.slug.valueOf());
-
-    while (mySlugArray.includes(myString)) {
-        myString = `${slug}-${counter}`
-        if (!mySlugArray.includes(myString)) {
-            slug = myString;
-        } else {
-            counter++;
-        }
-    };
+    while (existingSlugs.includes(slug)) slug = `${title}-${counter++}`;
 
     return slug;
 };
